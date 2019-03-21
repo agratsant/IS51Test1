840faff771b6a861680da3c8c9daa1db8ef05342
santsaxena <ags51197@gmail.com>
2019-03-20 17:42:20 -0700

    Made Changes in Exam-1

--- src/app/orders/orders.component.ts
+++ src/app/orders/orders.component.ts
@@ -21,7 +21,34 @@ export class OrdersComponent implements OnInit {
   }
 
   async ngOnInit() {
-
+    this.loadDefaultOrders();
   }
 
+
+
+loadDefaultOrders() {
+  this.orders = [{
+    'pid': '1',
+    'image': 'assets/sm_android.jpeg',
+    'description': 'Android',
+    'price': 150.00,
+    'quantity': 2
+  }, {
+    'pid': '2',
+    'image': 'assets/sm_iphone.jpeg',
+    'description': 'IPhone',
+    'price': 200.00,
+    'quantity': 1
+  }, {
+    'pid': '3',
+    'image': 'assets/sm_windows.jpeg',
+    'description': 'Windows Phone',
+    'price': 110.00,
+    'quantity': 2
+  }];
+}
+
+
+delete(index: number) {
+  console.log('from delete() index is: ', index);
 }
